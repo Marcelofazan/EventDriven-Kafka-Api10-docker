6829051b16866a7703688e252bc6e8a6875b0d85
Marcelofazan <56393266+Marcelofazan@users.noreply.github.com>
2026-06-09 13:52:37 -0300

    Add files via upload

---
+++ exemploApiKafka/Program.cs
@@ -0,0 +1,14 @@
+using exemploApiKafka.Services;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<ProducerServices>();
+
+var app = builder.Build();
+
+app.MapPost("/", async ([FromServices] ProducerServices services, [FromQuery] string message) =>
+{
+    return await services.SendMessage(message);
+});
+
+app.Run();
